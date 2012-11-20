f8db68b221307dc1c6bb7b44ef611e1860967e8f
Richard Wossal <richard@r-wos.org>
2012-11-20 21:13:19 +0100

    redirecting the old blog urls

---
+++ static/blog/out/index.php
@@ -0,0 +1,14 @@
+<?php
+// redirecting the old blog posts to the new location
+// (blog.r-wos.org/YEAR/POST --> r-wos.org/blog/POST)
+// (yes, that's hacky)
+$uri = $_GET['uri'];
+$uri = str_replace('2011/', '', $uri);
+$uri = str_replace('2012/', '', $uri);
+$uri = str_replace('.html', '', $uri);
+$uri = str_replace('.gz', '', $uri);
+
+header('HTTP/1.1 301 Moved Permanently');
+header('Location: http://r-wos.org/blog/' . $uri);
+exit;
+
