780550217342263daac818accc188fccce42fd96
Richard Wossal <richard@r-wos.org>
2012-11-21 16:59:33 +0100

    fixed trailing-slash handling for redirect script

--- static/blog/out/index.php
+++ static/blog/out/index.php
@@ -7,6 +7,7 @@ $uri = str_replace('2011/', '', $uri);
 $uri = str_replace('2012/', '', $uri);
 $uri = str_replace('.html', '', $uri);
 $uri = str_replace('.gz', '', $uri);
+$uri = rtrim($uri, '/');
 
 header('HTTP/1.1 301 Moved Permanently');
 header('Location: http://r-wos.org/blog/' . $uri);
