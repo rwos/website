64b2cdfa6825e716c0202f164c20c1091d093e13
Richard Wossal <richard@r-wos.org>
2022-04-16 13:55:35 +0200

    move stuff around

--- src/main.go
+++ src/main.go
@@ -17,6 +17,8 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -32,6 +34,9 @@ import (
 type PageConfig struct {
 	Title string
 }
+
+const contentBase = "content/"
+const buildBase = "../build/"
 
 func main() {
 	// TODO: https://github.com/abhinav/goldmark-toc
@@ -55,13 +60,45 @@ func main() {
 		),
 	)
 
-	files, err := filepath.Glob("./content/**")
+	files := []string{}
+	dirs := []string{}
+	err := filepath.Walk(contentBase, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		} else if path != contentBase {
+			path = strings.Replace(path, contentBase, "", 1)
+			dirs = append(dirs, path)
+		}
+		return nil
+	})
 	fatalIfError(err)
+
+	for _, p := range dirs {
+		fullPath := path.Join(buildBase, p)
+		fmt.Printf("creating dir %s\n", fullPath)
+		err := os.MkdirAll(fullPath, 0755)
+		fatalIfError(err)
+	}
+
 	for _, p := range files {
-		fmt.Printf("processing %s\n", p)
+		dest := strings.Replace(p, contentBase, buildBase, 1)
+		dest = strings.Replace(dest, ".md", ".html", 1)
+		if !strings.HasSuffix(p, ".md") {
+			fmt.Printf("copying %s to %s\n", p, dest)
+			input, err := ioutil.ReadFile(p)
+			fatalIfError(err)
+			err = ioutil.WriteFile(dest, input, 0644)
+			fatalIfError(err)
+			continue
+		}
+
+		fmt.Printf("rendering %s to %s\n", p, dest)
 		b, err := ioutil.ReadFile(p)
 		fatalIfError(err)
-		contents := strings.SplitN(string(b), "---", 2)
+		contents := strings.SplitN(string(b), "\n---\n", 2)
 		config := PageConfig{}
 		body := contents[0]
 		if len(contents) == 2 {
@@ -70,11 +107,12 @@ func main() {
 			body = contents[1]
 		}
 		// XXX defaults for page config, title etc
-		fmt.Printf(">>%+v -- %+v\n", body, config)
+		//fmt.Printf(">>%+v -- %+v\n", body, config)
 		var buf bytes.Buffer
 		err = md.Convert([]byte(body), &buf)
 		fatalIfError(err)
-		fmt.Printf(">>>rendered\n%s", buf.String())
+		err = ioutil.WriteFile(dest, buf.Bytes(), 0644)
+		fatalIfError(err)
 	}
 }
 
